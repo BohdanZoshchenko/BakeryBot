678da718d806204a8c3e9b61438350dcbad2e793
BohdanKo <zoschenkobohdan@gmail.com>
2021-05-26 09:10:54 +0300

    fixed floating db creds

--- db_helper.py
+++ db_helper.py
@@ -7,38 +7,42 @@ conn_params = None
 
 def do_sql(sql:str, fields=None, records_count_to_fetch:int=None):
     result = None
-    with connect(host=conn_params["host"],\
-        database=conn_params["database"],\
-        user=conn_params["user"],\
-        port=conn_params["port"],\
-        password=conn_params["password"])\
-    as conn: 
+    conn = None
+    if "HEROKU" in list(os.environ.keys()):
+        DATABASE_URL = os.environ['DATABASE_URL']
+        conn = connect(DATABASE_URL, sslmode='require')
+    else:
+        conn = connect(host=conn_params["host"],\
+            database=conn_params["database"],\
+            user=conn_params["user"],\
+            port=conn_params["port"],\
+            password=conn_params["password"])
         #print("Connected to DB")
-        with conn.cursor() as cursor:
+    with conn.cursor() as cursor:
             #print("DB: cursor created")
-            if fields != None:
-                cursor.execute(sql, fields)
-            else:
-                cursor.execute(sql)
+        if fields != None:
+            cursor.execute(sql, fields)
+        else:
+            cursor.execute(sql)
             #print("DB: query execution was succesful")
             #print(sql)
-            try:
-                if not records_count_to_fetch:
-                    result = cursor.fetchall()
-                elif records_count_to_fetch > 0:
-                    result = cursor.fetchmany(records_count_to_fetch)
+        try:
+            if not records_count_to_fetch:
+                result = cursor.fetchall()
+            elif records_count_to_fetch > 0:
+                result = cursor.fetchmany(records_count_to_fetch)
                 #else:
                    #print("DB: records count variable should be more than zero")
-            except:
-                pass
-        conn.commit()
+        except:
+            pass
+    conn.commit()
 
         #print("DB: commit was succesful")
         #if isinstance(result, list):
         #   print("DB: query result: " + str(result))
-        cursor.close()
+    cursor.close()
 
-        return result
+    return result
 
 def create_tables():
     create_tables = bot_tree["database"]["create_tables"]
